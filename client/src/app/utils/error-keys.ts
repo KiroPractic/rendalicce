04844ca72f4a1a4ec81dc378fe077f2548086296
DBDoco <dbdoco@gmail.com>
2024-10-10 19:47:46 +0200

    Fix error handling.

--- client/src/app/utils/error-keys.ts
+++ client/src/app/utils/error-keys.ts
@@ -4,8 +4,9 @@ export const errorKeys = (obj: { [key: string]: string[] }): string[] => {
   for (const key in obj) {
     if (obj.hasOwnProperty(key)) {
       const values = obj[key];
-      result.push(values["key"])
+      result.push(...values);
     }
   }
+
   return result;
-}
+};
