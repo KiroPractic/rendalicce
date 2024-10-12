54c9a3a2291c0bbbb94ca5883793a9050240e0a5
Tonči Marinac <toncimarinac@gmail.com>
2024-10-12 23:45:35 +0200

    WIP.

--- client/src/app/pages/profile/profile.component.ts
+++ client/src/app/pages/profile/profile.component.ts
@@ -45,6 +45,7 @@ export class ProfileComponent {
       const userId = params.id;
 
       this.service.getAccountInformation(userId).subscribe((user: User) => {
+        console.log(user);
         this.user = user;
       });
     });
