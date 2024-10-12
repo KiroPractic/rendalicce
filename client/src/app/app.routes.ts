fc174282d99d19deedb55ea3c566da7a552638c2
DBDoco <dbdoco@gmail.com>
2024-10-12 13:35:11 +0200

    WIP.

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,11 +1,10 @@
-import { Routes } from '@angular/router';
-import { LoginComponent } from './pages/authentication/login/login.component';
-import { HomeComponent } from './pages/home/home.component';
-import { RegisterComponent } from './pages/authentication/register/register.component';
-import { authenticationGuard } from './guards/authentication.guard';
+import {Routes} from '@angular/router';
+import {LoginComponent} from './pages/authentication/login/login.component';
+import {HomeComponent} from './pages/home/home.component';
+import {RegisterComponent} from './pages/authentication/register/register.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent, canActivate: [authenticationGuard] },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
+  {path: '', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
 ];
