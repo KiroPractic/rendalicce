3db4209744c8f69605b7e7853585baed3371c6a5
Franko Kirincic <frankokirincic@yahoo.com>
2024-10-12 22:37:45 +0200

    Add phone number to user.

---
+++ server/Rendalicce/Features/App/Chats/CreateChat.cs
@@ -0,0 +1,33 @@
+﻿// using FastEndpoints;
+// using Rendalicce.Persistency;
+//
+// namespace Rendalicce.Features.App.Chats;
+//
+// public sealed class GetChatWithUser
+// {
+//     public sealed record GetChatWithUserRequest(Guid UserId);
+//
+//     public sealed record GetChatWithUserResponse();
+//
+//     public sealed class GetChatWithUserEndpoint : Endpoint<GetChatWithUserRequest, Result<GetChatWithUserResponse>>
+//     {
+//         public required DatabaseContext DbContext { get; init; }
+//
+//         public override void Configure()
+//         {
+//             Get("chats/users/{userId}");
+//         }
+//
+//         public override async Task HandleAsync(GetChatWithUserRequest request, CancellationToken ct)
+//         {
+//             await SendAsync(new(new()), cancellation: ct);
+//         }
+//     }
+//
+//     public sealed class GetChatWithUserValidator : Validator<GetChatWithUserRequest>
+//     {
+//         public GetChatWithUserValidator()
+//         {
+//         }
+//     }
+// }
