f992542da489e525fbc7399c0bb6c98071a7496c
Paolo <kos.paolo@gmail.com>
2024-10-12 14:34:19 +0200

    Added about page

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { LoginComponent } from './pages/authentication/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { RegisterComponent } from './pages/authentication/register/register.component';
 import { ProfileComponent } from './pages/profile/profile.component';
+import { AboutComponent } from './pages/about/about.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'profile/:id', component: ProfileComponent },
+  { path: 'about', component: AboutComponent },
 ];
