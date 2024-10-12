8d3ba175ae9627b496ccda1cb84fcf22dfe601f3
Paolo <kos.paolo@gmail.com>
2024-10-12 14:23:38 +0200

    Router

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -4,10 +4,6 @@ import { HomeComponent } from './pages/home/home.component';
 import { RegisterComponent } from './pages/authentication/register/register.component';
 import { authenticationGuard } from './guards/authentication.guard';
 import { ProfileComponent } from './pages/profile/profile.component';
-import {Routes} from '@angular/router';
-import {LoginComponent} from './pages/authentication/login/login.component';
-import {HomeComponent} from './pages/home/home.component';
-import {RegisterComponent} from './pages/authentication/register/register.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, canActivate: [authenticationGuard] },
