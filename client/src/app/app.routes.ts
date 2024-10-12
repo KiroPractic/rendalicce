da2764310d1c6a44730b376817712d2814a014af
DBDoco <dbdoco@gmail.com>
2024-10-12 16:33:39 +0200

    Conflict fix.

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -5,13 +5,7 @@ import { RegisterComponent } from './pages/authentication/register/register.comp
 import { ProfileComponent } from './pages/profile/profile.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
-import {Routes} from '@angular/router';
-import {LoginComponent} from './pages/authentication/login/login.component';
-import {HomeComponent} from './pages/home/home.component';
-import {RegisterComponent} from './pages/authentication/register/register.component';
-import {
-  CreateOrUpdateServiceProviderComponent
-} from "./pages/create-or-update-service-provider/create-or-update-service-provider.component";
+import {CreateOrUpdateServiceProviderComponent} from "./pages/create-or-update-service-provider/create-or-update-service-provider.component";
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
