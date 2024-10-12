739e754ed13f9c16c71dd74f57bbf84405868965
Tonči Marinac <toncimarinac@gmail.com>
2024-10-12 23:31:23 +0200

    WIP.

--- client/src/app/pages/profile/profile.service.ts
+++ client/src/app/pages/profile/profile.service.ts
@@ -2,6 +2,7 @@ import {inject, Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../../../environments/environment";
 import {ApiRoutes} from "../../enums/api-routes.enum";
+import {User} from "../../model/user.model";
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,9 @@ import {ApiRoutes} from "../../enums/api-routes.enum";
 export class ProfileService {
   #httpClient: HttpClient = inject(HttpClient);
 
+  getUser() {
+    return this.#httpClient.get<User>(`${environment.baseUrl}${ApiRoutes.apiRoute}${ApiRoutes.accountRoute}`);
+  }
   getAccountInformation(id: string) {
     return this.#httpClient.get(`${environment.baseUrl}${ApiRoutes.apiRoute}${ApiRoutes.usersRoute}/${id}`);
   }
