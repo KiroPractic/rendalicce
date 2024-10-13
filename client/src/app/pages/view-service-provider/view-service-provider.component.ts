ab3827ba011fa83d058ddb23fad5e98afff096a9
DBDoco <dbdoco@gmail.com>
2024-10-13 07:46:52 +0200

    Add review upload logic.

--- client/src/app/pages/view-service-provider/view-service-provider.component.ts
+++ client/src/app/pages/view-service-provider/view-service-provider.component.ts
@@ -1,11 +1,11 @@
-import {Component, inject} from '@angular/core';
-import {ActivatedRoute, RouterLink} from "@angular/router";
-import {ViewServiceProviderService} from "./view-service-provider.service";
-import {DatePipe, DecimalPipe, NgForOf, NgIf} from "@angular/common";
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, RouterLink } from "@angular/router";
+import { ViewServiceProviderService } from "./view-service-provider.service";
+import { DatePipe, DecimalPipe, NgForOf, NgIf } from "@angular/common";
 import { DialogModule } from 'primeng/dialog';
 import { RatingModule } from 'primeng/rating';
 import { FormsModule } from '@angular/forms';
-import {ProgressSpinnerModule} from "primeng/progressspinner";
+import { ProgressSpinnerModule } from "primeng/progressspinner";
 
 @Component({
   selector: 'app-view-service-provider',
@@ -23,7 +23,7 @@ import {ProgressSpinnerModule} from "primeng/progressspinner";
     ProgressSpinnerModule
   ],
   templateUrl: './view-service-provider.component.html',
-  styleUrl: './view-service-provider.component.scss'
+  styleUrls: ['./view-service-provider.component.scss']
 })
 export class ViewServiceProviderComponent {
   activatedRoute: ActivatedRoute = inject(ActivatedRoute);
@@ -47,15 +47,42 @@ export class ViewServiceProviderComponent {
     });
   }
 
+  fetchServiceProvider(serviceId: string) {
+    this.isLoading = true;
+    this.service.getById(serviceId).subscribe((service: any) => {
+      this.serviceProvider = service.serviceProvider;
+      this.isLoading = false;
+    }, () => {
+      this.isLoading = false;
+    });
+  }
+
   onAddReview() {
     if (this.rating > 0 && this.reviewText) {
-      console.log('Rating:', this.rating);
-      console.log('Review:', this.reviewText);
+      const formData = new FormData();
+
+      formData.append('rating', this.rating.toString());
+      formData.append('content', this.reviewText);
+
+      if (this.image) {
+        formData.append('contentPhoto', this.image, this.image.name);
+      }
+
+      const serviceId = this.serviceProvider.id;
+
+      this.service.postReview(serviceId, formData).subscribe(() => {
+        console.log('Review submitted successfully.');
+
+        this.fetchServiceProvider(serviceId);
+
+      }, (error) => {
+        console.error('Error submitting review:', error);
+      });
 
       this.displayRatingModal = false;
-
       this.rating = 0;
       this.reviewText = '';
+      this.image = null;
     } else {
       alert('Please provide a rating and a review.');
     }
